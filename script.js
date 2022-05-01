5b106b9609eb80f62d925eeb96cb14f89d6f17c0
Jacob Sierra <jacobisaac22@yahoo.com>
2022-05-01 18:03:30 -0500

    first commit, player search and css update

---
+++ script.js
@@ -0,0 +1,96 @@
+const searchBtn = document.getElementById('btn');
+const input = document.getElementById('input');
+let image = document.querySelector('img')
+let playerName = document.querySelector('h3')
+let playNumber = document.querySelector('p')
+const stats = document.querySelector('.stats')
+const gamesPlayed = document.querySelector('.gamesPlayed')
+const seasonPts = document.querySelector('.seasonPts')
+const seasonAst = document.querySelector('.seasonAst')
+const seasonBlks = document.querySelector('.seasonBlks')
+const container = document.querySelector('.infoContainer')
+
+searchBtn.addEventListener('click', function () {
+  container.classList.remove('hide')
+	gamesPlayed.textContent = ''
+	seasonAst.textContent = ''
+	seasonPts.textContent = ''
+	seasonBlks.textContent = ''
+	fetch(`https://www.balldontlie.io/api/v1/players?search=${input.value}`)
+		.then(function (response) {
+			return response.json()
+		}).then(function (data) {
+			console.log(data)
+			playerName.textContent = [data.data[0].first_name, data.data[0].last_name].join(' ')
+		})
+  localStorage.setItem('player', input.value)
+	let array = [];
+	let playerSearch = input.value
+	let arrayPlayerSearch = playerSearch.split(' ')
+
+	return fetch('http://data.nba.net/data/10s/prod/v1/2021/players.json')
+		.then(function (response) {
+			return response.json()
+		}).then(function (data) {
+			array.push(data.league.standard)
+			for (i = 0; i < array[0].length; i++) {
+				let firstNameLower = array[0][i].firstName.toLowerCase()
+				let lastNameLower = array[0][i].lastName.toLowerCase()
+				let firstNameSearchLower = arrayPlayerSearch[0].toLowerCase()
+				let lastNameSearchLower = arrayPlayerSearch[1].toLowerCase()
+				if (firstNameLower === firstNameSearchLower && lastNameLower === lastNameSearchLower) {
+					let playerId = array[0][i].personId
+					image.src = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/" + playerId + ".png"
+					playNumber.textContent = '#' + array[0][i].jersey
+				}
+				else { }
+			}
+		})
+})
+
+
+
+stats.addEventListener('click', function () {
+	let statsArray = []
+	let playerId;
+
+let playerSearch2 = localStorage.getItem('player')
+console.log(playerSearch2)
+	let arrayPlayerSearch = playerSearch2.split(' ')
+console.log(arrayPlayerSearch)
+	fetch("https://www.balldontlie.io/api/v1/players?per_page=100&search=" + arrayPlayerSearch[1] + "")
+		.then(function (response) {
+			return response.json()
+		}).then(function (data) {
+			console.log(data)
+			statsArray.push(data.data)
+			console.log(statsArray)
+			for (i = 0; i < statsArray[0].length; i++) {
+				let firstNameLower = statsArray[0][i].first_name.toLowerCase()
+				let lastNameLower = statsArray[0][i].last_name.toLowerCase()
+				let firstNameSearchLower = arrayPlayerSearch[0].toLowerCase()
+				let lastNameSearchLower = arrayPlayerSearch[1].toLowerCase()
+				if (firstNameLower === firstNameSearchLower && lastNameLower === lastNameSearchLower) {
+					playerId = statsArray[0][i].id
+					console.log(playerId)
+
+
+				}
+				else { }
+			}
+
+			fetch(`https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${playerId}`)
+				.then(function (response) {
+					return response.json()
+				}).then(function (data) {
+					console.log(data)
+					gamesPlayed.textContent = 'Games Played: ' + data.data[0].games_played
+					seasonPts.textContent = 'Points per game: ' + data.data[0].pts
+					seasonAst.textContent = 'Assist per game: ' + data.data[0].ast
+					seasonBlks.textContent = 'Blocks per game: ' + data.data[0].blk
+				})
+		})
+
+})
+
+
